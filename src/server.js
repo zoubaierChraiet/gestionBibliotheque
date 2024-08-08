f5f4e2e6bcdd1aa3c7f4b8e7d20fc9a81521cf24
zoubaier chraiet <zouba.chraiet@yahoo.com>
2024-08-08 15:17:06 +0200

    added database connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
+// Librairies
 import express from "express";
+
+// Utils
+import connectDatabase from "./utils/db.js";
 
 const app = express();
 
 const PORT = 3000;
+
+connectDatabase();
 
 app.listen(PORT, () => {
   console.log(`App Started on port: ${PORT}`);
---
+++ src/utils/db.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected;
+
+const connectDatabase = async () => {
+  // check if db is already connected
+  if (isConnected) {
+    console.log("using current database connection");
+    return;
+  }
+
+  try {
+    // init db connection
+    const db = await mongoose.connect(
+      "mongodb://localhost:27017/gestionBibliotheque"
+    );
+    // update connection state
+    isConnected = db.connections[0].readyState;
+    console.log("database is connected");
+  } catch (err) {
+    console.error("Database connection error:", error);
+  }
+};
+
+export default connectDatabase;
